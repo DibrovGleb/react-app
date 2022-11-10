be11fcc765222d20f8f71f5d9e77fac319a36a2e
Gleb <g.t.ru@mail.ru>
2022-11-10 20:27:01 +0300

    basic set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Example from './components/example';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Example/>
         <p>Route, TS, MUI , Redux</p>
       </header>
     </div>
---
+++ src/components/example.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Example() {
+  return (
+    <div>
+    <h1>Im the function typescript component!</h1>
+    <h2>Yay</h2>
+    </div>
+  )
+}
+  // Easiest way to declare a Function Component; return type is inferred.
+//const Example = () => <h1>HELLO</h1>;
+
+export default Example
